76c767b0585546d2f253fba3485317260bb4c2e9
idand2 <idanday2@gmail.com>
2020-01-08 19:13:17 +0200

    doc file_handler

--- files_handler.py
+++ files_handler.py
@@ -1,11 +1,21 @@
 class FileHandler(object):
     @staticmethod
     def read_bytes(path):
+        """
+        read bytes from file
+        @param path: string, the path to the file
+        @return: file content as bytes
+        """
         with open(path, 'rb') as file:
             content_bytes = file.read()
         return content_bytes
 
     @staticmethod
     def write_bytes(path, content):
+        """
+        write bytes to file
+        @param path: string, path for saving the content
+        @param content: the content to write to file.
+        """
         with open(path, 'wb') as image_from_client:
             image_from_client.write(content)
